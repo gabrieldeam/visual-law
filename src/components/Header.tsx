d21e5657cba5244d7cfb498ca0dceba4d46f88ff
Gabriel De Almeida Mchado <81692046+gabrieldeam@users.noreply.github.com>
2024-09-19 19:23:54 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,6 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <h1>Visual Law</h1>
-      <nav className={styles.nav}>
-        <Link href="/">Início</Link> | 
-        <Link href="/sobre">Sobre</Link> | 
-        <Link href="/contato">Contato</Link>
-      </nav>
     </header>
   );
 };
